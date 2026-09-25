23f08376da5f7bded4b6b561298d5c891cd3826c
Jobayer Hossain <hossainjobayer.bd@gmail.com>
2026-09-25 18:07:58 +0600

    explore completed and worikng on the cardcollection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Suspense } from "react";
 import Banner from "./components/Banner";
 import Explore from "./components/Explore";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import type { Itechnology } from "./types/types";
+
+const technologyfetch = async (): Promise<Itechnology[]> => {
+	const res = await fetch("/public/data.json");
+	const data = await res.json();
+	return data;
+};
 
 function App() {
 	return (
@@ -9,6 +18,8 @@ function App() {
 				<Nav></Nav>
 				<Banner></Banner>
 				<Explore></Explore>
+				<Suspense fallback={<h2>Loading....</h2>}></Suspense>
+				<Footer></Footer>
 			</div>
 		</>
 	);
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Itechnology {
+	id: 1;
+	name: "React";
+	category: "Frontend";
+	description: "A JavaScript library for building interactive and component-based user interfaces.";
+	icon: "https://cdn.simpleicons.org/react/61DAFB";
+	rating: 4.8;
+	difficulty: "Intermediate";
+	badge: "Popular";
+}
